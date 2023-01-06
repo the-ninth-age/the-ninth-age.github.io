f87b56b0e2d20a786bdd4af37c708d69af2e2a32
Heko <122116383+the-ninth-age@users.noreply.github.com>
2023-01-06 19:55:09 +0100

    Add game.

---
+++ game.js
@@ -0,0 +1,41 @@
+$(() => {
+    console.log('start');
+
+    const config = {
+        type: Phaser.AUTO,
+        width: '100%',
+        height: '100%',
+        parent: 'canvas-container',
+        scale: {
+            mode: Phaser.Scale.RESIZE
+        },
+        scene: [GameScene]
+    };
+
+    new Phaser.Game(config);
+});
+
+class GameScene extends Phaser.Scene {
+
+    constructor() {
+        super(GameScene.name);
+    }
+
+    preload() {
+    }
+
+    create() {
+        this.graphics = this.add.graphics();
+
+        $(window).resize(() => this.drawGameAfterResize());
+    }
+
+    drawGameAfterResize() {
+        this.graphics.clear();
+    }
+
+    captureWindowSize() {
+        this.windowWidth = $(window).width();
+        this.windowHeight = $(window).height();
+    }
+}
